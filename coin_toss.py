d2820025490e45cfc803df75f8ad3e2f1247ff17
Anaya_Abro <abroayaan8@gmail.com>
2026-04-29 21:21:44 +0500

    Add files via upload

---
+++ coin_toss.py
@@ -0,0 +1,27 @@
+import random
+print('Welcome to Coin tossing game🪙\n')
+
+coin = ['heads','tails']
+while True:
+ user = input('please enter toss to toss the coin:').lower()
+#  result = random.choice
+ if user == 'toss':
+   result = random.choice(coin)
+   if result == 'heads':
+    print('you got heads🎉')
+
+   else:
+    print('you got tails🎉')
+
+
+
+   play_again = input('Do you want to play again(yes/no)').lower()
+   if play_again == 'no':
+    print('thank you for playing🎮 ')
+    break
+
+ else:
+  print('Invalid text! (please write toss)')
+
+
+
